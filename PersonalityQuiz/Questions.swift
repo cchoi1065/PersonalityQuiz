aac5f928e9c8680dfdbc2dd876b3f2da77a6ef38
Charles Choi <charleschoi@jh-mba158-06.d214.org>
2018-03-07 11:20:10 -0600

    added Animal enumeration

--- PersonalityQuiz/Questions.swift
+++ PersonalityQuiz/Questions.swift
@@ -23,5 +23,20 @@ struct Answer {
     var type : AnimalType
 }
 
+enum AnimalType: Character {
+    case dog = "🐶", cat = "😺", rabbit = "🐰", turtle = "🐢"
+    var definition: String {
+        switch self {
+        case .dog:
+            return "You are incredibly outgoing. You surround yourself with people you love, and enjoy activites with your friends"
+        case .cat:
+            return "Mischievous, yet mild-tempered, you enjoy doing things on your own terms"
+        case .rabbit:
+            return "You love everything that's soft. Your are healthy and full of energy"
+        case .turtle:
+            return "You are wise beyond your years, and you focus on the details. Slow and steady wins the race."
+        }
+    }
+}
 
 
