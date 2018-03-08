b60ec6183745dd3009f26003d9d313f49f3508fb
Charles Choi <charleschoi@jh-mba158-06.d214.org>
2018-03-08 11:26:15 -0600

    InitialViewController is adjusted

--- PersonalityQuiz/QuestionsViewController.swift
+++ PersonalityQuiz/QuestionsViewController.swift
@@ -10,26 +10,40 @@ import UIKit
 
 class QuestionsViewController: UIViewController {
 
+    
+    var questionIndex = 0
+    var questions: [Question] = [
+        Question(text: "Which food do you like the most?",
+                 type: .single,
+                 answers: [
+                    Answer(text: "Steak", type: .dog),
+                    Answer(text: "Fish", type: .cat),
+                    Answer(text: "Carrot", type: .rabbit),
+                    Answer(text: "Lettuce", type: .turtle),
+                    ]),
+        
+        
+        Question(text: "Which activities do you like the most?",
+                 type: .multiple,
+                 answers: [
+                    Answer(text: "Swimming", type: .dog),
+                    Answer(text: "Sleeping", type: .cat),
+                    Answer(text: "Cuddling", type: .rabbit),
+                    Answer(text: "Eating", type: .turtle),
+                    ]),
+        
+        
+        Question(text: "How much do you enjoy car rides?",
+                 type: .ranged,
+                 answers: [
+                    Answer(text: "I love them", type: .dog),
+                    Answer(text: "I dislike them", type: .cat),
+                    Answer(text: "I get a little nervous", type: .rabbit),
+                    Answer(text: "I barley notice them", type: .turtle),
+                    ])
+    ]
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+}
 }
